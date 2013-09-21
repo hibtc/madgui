89f49e314a9ca80d1618b18c3c26ff7c574cfdeb
Thomas Gläßle <t_glaessle@gmx.de>
2013-09-21 19:00:53 +0200

    Undo add local pathes to sys.path

--- run.py
+++ run.py
@@ -6,14 +6,6 @@ import inspect
 import os
 import sys
 
-# add local lib pathes
-_file = inspect.getfile(inspect.currentframe())
-_path = os.path.realpath(os.path.abspath(os.path.dirname(_file)))
-for lib in ['event']:
-    _subm = os.path.join(_path, 'lib', lib)
-    if _subm not in sys.path:
-        sys.path.insert(0, _subm)
-
 import madgui.main
 
 if __name__ == '__main__':
