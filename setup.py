d7b1bfb3819272d1745f8ce0e1be07577b6bf2c2
Thomas Gläßle <t_glaessle@gmx.de>
2013-09-21 18:16:56 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from setuptools import setup
+
+# Read README.rst for PyPI
+# you should convert it by hand like so: pandoc README.md -o README.rst
+try:
+    f = open('README.rst')
+    long_description = f.read()
+    f.close()
+except:
+    long_description = None
+
+
+setup(name='madgui',
+    version='0.0.1',
+    description='GUI for beam simulation using MadX via PyMad',
+    long_description=long_description,
+    author='Thomas Gläßle',
+    author_email='t_glaessle@gmx.de',
+    maintainer='Thomas Gläßle',
+    maintainer_email='t_glaessle@gmx.de',
+    url=None,
+    py_modules=[
+        'madgui',
+        'common',
+        'controller',
+        'model',
+        'view',
+        ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Healthcare Industry',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Medical Science Apps.',
+        'Topic :: Scientific/Engineering :: Physics',
+    ],
+    license=None,
+    test_suite='nose.collector',
+    install_requires=['pydicti','cern-pymad',],
+    )
