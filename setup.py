a6690e882e91c56d41e946a31e2009a12141fed7
Thomas Gläßle <t_glaessle@gmx.de>
2013-09-21 18:42:06 +0200

    Fix name of pymad dependency

--- setup.py
+++ setup.py
@@ -35,5 +35,5 @@ setup(name='madgui',
     ],
     license=None,
     test_suite='nose.collector',
-    install_requires=['pydicti','cern-pymad',],
+    install_requires=['pydicti','PyMAD',],
     )
