0f39f4328251b71f98f9e2d39d4bc6bc42a1e6f0
Thomas Gläßle <t_glaessle@gmx.de>
2013-12-28 14:48:03 +0100

    Convert README to reST

---
+++ README.rst
@@ -0,0 +1,26 @@
+MadGUI
+------
+
+Description
+~~~~~~~~~~~
+
+MadGUI is a python GUI for accelerator simulations using MAD-X_. Currently
+it is rather basic and can't be used outside a very limited scope.
+
+.. _MAD-X: http://madx.web.cern.ch/madx
+
+Dependencies
+~~~~~~~~~~~~
+
+-  MAD-X_ as dynamic library (``.dll`` or ``.so``)
+-  cpymad_
+-  numpy_
+-  obsub_ (automatically resolved via setuptools)
+-  pydicti_ (automatically resolved via setuptools)
+
+.. _MAD-X: http://madx.web.cern.ch/madx
+.. _cpymad: http://pymad.github.io
+.. _numpy: http://www.numpy.org
+.. _obsub: https://github.com/aepsil0n/obsub
+.. _pydicti: https://github.com/coldfix/pydicti
+
--- setup.py
+++ setup.py
@@ -3,26 +3,17 @@
 
 from setuptools import setup
 
-# Read README.rst for PyPI
-# you should convert it by hand like so: pandoc README.md -o README.rst
-try:
-    f = open('README.rst')
-    long_description = f.read()
-    f.close()
-except:
-    long_description = None
-
-
-setup(name='madgui',
+setup(
+    name='madgui',
     version='0.2',
     description='GUI for beam simulation using MadX via PyMad',
-    long_description=long_description,
+    long_description=open('README.rst').read(),
     author='Thomas Gläßle',
     author_email='t_glaessle@gmx.de',
     maintainer='Thomas Gläßle',
     maintainer_email='t_glaessle@gmx.de',
-    url=None,
-    packages=['madgui',],
+    url='https://github.com/coldfix/madgui',
+    packages=['madgui'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
@@ -38,7 +29,8 @@ setup(name='madgui',
     install_requires=[
         'pydicti',
         'obsub',
-        'PyMAD',],
+        'PyMAD',
+    ],
     entry_points={
         'gui_scripts': [
             'madgui = madgui.main:main'
@@ -46,5 +38,5 @@ setup(name='madgui',
     },
     package_data={
         'madgui': ['resource/*']
-        }
-    )
+    }
+)
