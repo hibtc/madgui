4477f9db20b2158be81cc66edd7c58b514119c37
Thomas Gläßle <t_glaessle@gmx.de>
2014-03-23 00:27:27 +0100

    Remove unused functions: rchop, axis_name

--- madgui/common.py
+++ madgui/common.py
@@ -10,18 +10,6 @@ import os
 import functools
 
 
-def rchop(thestring, ending):
-    """Remove substring at the end of a string."""
-    if thestring.endswith(ending):
-        return thestring[:-len(ending)]
-    return thestring
-
-
-def axis_name(axis_num):
-    """Return readable name corresponding to axis number."""
-    return "xyz"[axis_num]
-
-
 def makedirs(path):
     """Make sure 'path' exists. Like 'os.makedirs(path, exist_ok=True)'."""
     try:
