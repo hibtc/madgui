830e6fde65e7fded216200ac1cd76c291021dc78
Thomas Gläßle <t_glaessle@gmx.de>
2014-03-22 19:55:57 +0100

    Connect capture_mouse with Zoom/Pan toolbar actions

--- madgui/mainframe.py
+++ madgui/mainframe.py
@@ -52,6 +52,35 @@ class ViewPanel(wx.Panel):
         sizer.Add(self.toolbar, 0 , wx.LEFT | wx.EXPAND)
         self.SetSizer(sizer)
 
+        self.capturing = False
+        self.hook.capture_mouse.connect(self.on_capture_mouse)
+
+        self.toolbar.Bind(wx.EVT_TOOL,
+                          self.on_zoom_or_pan,
+                          id=self.toolbar.wx_ids['Zoom'])
+        self.toolbar.Bind(wx.EVT_TOOL,
+                          self.on_zoom_or_pan,
+                          id=self.toolbar.wx_ids['Pan'])
+
+    def on_zoom_or_pan(self, event):
+        if event.IsChecked():
+            self.capturing = True
+            self.hook.capture_mouse()
+            self.capturing = False
+        event.Skip()
+
+    def on_capture_mouse(self):
+        if self.capturing:
+            return
+        zoom_id = self.toolbar.wx_ids['Zoom']
+        if self.toolbar.GetToolState(zoom_id):
+            self.toolbar.zoom()
+            self.toolbar.ToggleTool(zoom_id, False)
+        pan_id = self.toolbar.wx_ids['Pan']
+        if self.toolbar.GetToolState(pan_id):
+            self.toolbar.pan()
+            self.toolbar.ToggleTool(pan_id, False)
+
     def OnPaint(self, event):
         """Handle redraw by painting canvas."""
         self.canvas.draw()
