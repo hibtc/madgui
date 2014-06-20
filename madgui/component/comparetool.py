2433372f28b08db52c36ffd9016726aba9205b31
Thomas Gläßle <t_glaessle@gmx.de>
2014-06-20 15:43:23 +0200

    Use more metadata for comparetool
    Resolves #47

--- madgui/component/comparetool.py
+++ madgui/component/comparetool.py
@@ -11,10 +11,31 @@ import numpy as np
 
 # internal
 from madgui.core import wx
-from madgui.util.unit import units, strip_unit
+from madgui.util import unit
 
 # exported symbols
 __all__ = ['CompareTool']
+
+
+def match_metadata(all_metadata, sequence, col_names):
+    """
+    Find suitable match for sequence/optic/range/columns in metadata.
+    
+    :param list all_metadata: all metadata entries for the model
+    :param str sequence: sequence name to be searched for
+    :param list col_names: column names to be searched for
+    :returns: matched metadata
+    :rtype: dict
+
+    TODO: Add optic/range matching
+    """
+    for review in all_metadata:
+        if review['sequence'] != sequence:
+            continue
+        columns = review['columns']
+        if all(col in columns for col in col_names):
+            return review
+    raise ValueError()
 
 
 class CompareTool(object):
@@ -32,35 +53,43 @@ class CompareTool(object):
 
         The envelope is NOT visible by default.
         """
-        self.model = panel.view.model
-        self.view = panel.view
-        self.lines = {}
+        self._view = view = panel.view
+        self._model = model = view.model
+        self._lines = {}
         self._visible = False
-        if self.test_file:
-            # connect to toolbar
-            bmp = wx.ArtProvider.GetBitmap(wx.ART_GO_HOME, wx.ART_TOOLBAR)
-            tool = panel.toolbar.AddCheckTool(
-                    wx.ID_ANY,
-                    bitmap=bmp,
-                    shortHelp='Show MIRKO envelope',
-                    longHelp='Show MIRKO envelope for comparison. The envelope is computed for the default parameters.')
-            panel.Bind(wx.EVT_TOOL, self.OnMirkoClick, tool)
-            # subscribe to plotting
-            self.view.hook.plot_ax.connect(self.plot_ax)
+        self._metadata = None
 
-    def OnMirkoClick(self, event):
+        cpymad_model = model.model
+        if not cpymad_model:
+            return
+
+        all_metadata = cpymad_model.mdef['review']
+        col_names = [view.sname, view.xname, view.yname]
+        try:
+            metadata = match_metadata(all_metadata, model.name, col_names)
+        except ValueError:
+            return
+        self._metadata = metadata
+
+        # connect to toolbar
+        bmp = wx.ArtProvider.GetBitmap(wx.ART_GO_HOME, wx.ART_TOOLBAR)
+        tool = panel.toolbar.AddCheckTool(
+                wx.ID_ANY,
+                bitmap=bmp,
+                shortHelp='Show MIRKO envelope',
+                longHelp='Show MIRKO envelope for comparison. The envelope is computed for the default parameters.')
+        panel.Bind(wx.EVT_TOOL, self.on_click, tool)
+        # subscribe to plotting
+        view.hook.plot_ax.connect(self.plot_ax)
+
+    def on_click(self, event):
         """Invoked when user clicks Mirko-Button"""
         self.visible = event.IsChecked()
 
     @property
     def test_file(self):
         """Get the envelope file."""
-        model = self.model.model
-        if not model:
-            return None
-        optic = model._mdef['optics'][model._active['optic']]
-        if 'test' in optic:
-            return model.mdata.get_by_dict(optic['test'])
+        return self._model.model.mdata.get_by_dict(self._metadata['file'])
 
     @property
     def visible(self):
@@ -69,8 +98,9 @@ class CompareTool(object):
 
     @visible.setter
     def visible(self, visible):
+        """Set visibility."""
         self._visible = visible
-        view = self.view
+        view = self._view
         xname = view.xname
         yname = view.yname
         if visible:
@@ -79,16 +109,18 @@ class CompareTool(object):
         else:
             self._remove_ax(xname)
             self._remove_ax(yname)
-        self.view.figure.canvas.draw()
+        self._view.figure.canvas.draw()
 
-    def load_data(self):
+    def load_data(self, name):
+        """Load envelope from file."""
+        column_info = self._metadata['columns']
+        scol = column_info['s']
+        ycol = column_info[name]
         with self.test_file.filename() as f:
-            aenv = units.mm * np.loadtxt(f, usecols=(0,1,2))
-        view = self.view
+            aenv = np.loadtxt(f, usecols=(scol['column'], ycol['column']))
         return {
-            view.sname: aenv[:,0],
-            view.xname: aenv[:,1],
-            view.yname: aenv[:,2]
+            's': unit.from_config(scol['unit']) * aenv[:,0],
+            name: unit.from_config(ycol['unit']) * aenv[:,1],
         }
 
     def plot_ax(self, axes, name):
@@ -96,15 +128,15 @@ class CompareTool(object):
         if not self.visible:
             return
         self._remove_ax(name)
-        view = self.view
-        envdata = self.load_data()
+        view = self._view
+        envdata = self.load_data(name)
         sname = view.sname
-        self.lines[name] = axes.plot(
-            strip_unit(envdata[sname], self.view.unit[sname]),
-            strip_unit(envdata[name], self.view.unit[name]),
+        self._lines[name] = axes.plot(
+            unit.strip_unit(envdata[sname], view.unit[sname]),
+            unit.strip_unit(envdata[name], view.unit[name]),
             'k')
 
     def _remove_ax(self, name):
         """Remove the envelope from the figure."""
-        for l in self.lines.pop(name, []):
+        for l in self._lines.pop(name, []):
             l.remove()
