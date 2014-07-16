2f206d9b1f248f2f246b9d76a8ab714ea5fe41c1
Thomas Gläßle <t_glaessle@gmx.de>
2014-07-16 19:28:00 +0200

    Minor changes to line spacing in setup.py

--- setup.py
+++ setup.py
@@ -7,12 +7,14 @@ Usage:
     python setup.py install
 """
 
+
 from setuptools import setup
-import madgui
+from madgui import __version__
+
 
 setup(
     name='madgui',
-    version=madgui.__version__,
+    version=__version__,
     description='GUI for beam simulation using MadX via PyMad',
     long_description=open('README.rst').read(),
     author='Thomas Gläßle',
