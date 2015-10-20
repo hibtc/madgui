65d2041ce880a1fbba447951e2c8e81407436b0d
Thomas Gläßle <t_glaessle@gmx.de>
2015-10-20 19:17:55 +0200

    Add preliminary dual slider widget
    Will be used for the new Range selection widget.

---
+++ madgui/widget/slider.py
@@ -0,0 +1,82 @@
+"""
+Widget for dual slider.
+"""
+
+from __future__ import absolute_import
+
+from .input import Widget
+import wx
+
+from wx.lib.newevent import NewEvent
+
+
+RangeChange, EVT_RANGE_CHANGE = NewEvent()
+RangeChangeStart, EVT_RANGE_CHANGE_START = NewEvent()
+RangeChangeStop, EVT_RANGE_CHANGE_STOP = NewEvent()
+
+
+class DualSlider(Widget):
+
+    """
+    Widget class to select a range.
+    """
+
+    # TODO:
+    # - implement in terms of single two-pin slider control
+
+    def __init__(self, parent, info_min, info_max):
+        self.info_min = info_min
+        self.info_max = info_max
+        super(DualSlider, self).__init__(parent)
+
+    def CreateControls(self, window):
+        self.ctrl_start = start = wx.Slider(window, style=wx.SL_TOP)
+        self.ctrl_stop = stop = wx.Slider(window, style=wx.SL_BOTTOM)
+
+        sizer = wx.FlexGridSizer(rows=2, cols=2, vgap=5, hgap=5)
+        sizer.SetFlexibleDirection(wx.HORIZONTAL)
+        sizer.AddGrowableCol(1)
+
+        sizer.Add(self.info_min)
+        sizer.Add(start, 1, wx.ALL|wx.ALIGN_CENTER_VERTICAL|wx.EXPAND)
+        sizer.Add(self.info_max)
+        sizer.Add(stop, 1, wx.ALL|wx.ALIGN_CENTER_VERTICAL|wx.EXPAND)
+
+        start.Bind(wx.EVT_SLIDER, self.OnSliderStart)
+        stop.Bind(wx.EVT_SLIDER, self.OnSliderStop)
+        return sizer
+
+    def SetData(self, cur, limits=None):
+        if limits is not None:
+            min, max = limits
+            self.ctrl_start.SetMin(min)
+            self.ctrl_stop.SetMin(min)
+            self.ctrl_start.SetMax(max)
+            self.ctrl_stop.SetMax(max)
+        start, stop = cur
+        self.ctrl_start.SetValue(start)
+        self.ctrl_stop.SetValue(stop)
+
+    def GetData(self):
+        return (self.ctrl_start.GetValue(),
+                self.ctrl_stop.GetValue())
+
+    def OnSliderStart(self, event):
+        start, stop = self.GetData()
+        self._Post(RangeChangeStart, start=start, stop=stop)
+        if start > stop:
+            self.ctrl_stop.SetValue(start)
+            self._Post(RangeChangeStop, start=start, stop=stop)
+        self._Post(RangeChange, start=start, stop=stop)
+
+    def OnSliderStop(self, event):
+        start, stop = self.GetData()
+        self._Post(RangeChangeStop, start=start, stop=stop)
+        if stop < start:
+            self.ctrl_start.SetValue(stop)
+            self._Post(RangeChangeStart, start=start, stop=stop)
+        self._Post(RangeChange, start=start, stop=stop)
+
+    def _Post(self, event_class, **args):
+        event = event_class(**args)
+        wx.PostEvent(self.Window.GetEventHandler(), event)
