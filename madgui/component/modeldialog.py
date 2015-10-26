bfb433b60d22ba66839fb86d0dc6c46aff9f01f1
Thomas Gläßle <t_glaessle@gmx.de>
2015-10-26 17:18:38 +0100

    Update model widget on sequence selection

--- madgui/component/modeldialog.py
+++ madgui/component/modeldialog.py
@@ -51,39 +51,61 @@ class ModelWidget(Widget):
         sizer.Add(wx.StaticLine(window, style=wx.LI_HORIZONTAL), 0, wx.EXPAND)
         sizer.Add(l_twiss, 0, wx.ALL|wx.ALIGN_LEFT, 5)
         sizer.Add(w_twiss.Control, 1, wx.ALL|wx.EXPAND, 5)
+        w_sequence.Control.Bind(wx.EVT_CHOICE, self.OnChangeSequence)
         self.w_sequence = w_sequence
         self.w_beam = w_beam
         self.w_twiss = w_twiss
         return sizer
 
-    def SetData(self, mdata):
-        if mdata is None:
-            self.mdata = Model.detect(self.session.madx)[0]
+    def OnChangeSequence(self, event=None):
+        """
+        Update UI controls after changing the sequence selection using the
+        most suitable known model data for the given sequence.
+        """
+        sequence, _ = self.w_sequence.GetData()
+        if self.mdata.get('sequence') == sequence:
+            mdata = self.mdata
         else:
-            self.mdata = mdata
-        self.w_beam.SetData(self.mdata.get('beam', {}))
-        self.w_twiss.SetData(self.mdata.get('twiss', {}))
-        self._UpdateSequence()
+            mdata = Model.get_seq_model(self.session.madx,
+                                        self.session.utool,
+                                        sequence)
+        self.w_sequence._UpdateRange()
+        self.w_beam.SetData(mdata.get('beam', {}))
+        self.w_twiss.SetData(mdata.get('twiss', {}))
+
+    def SetData(self, mdata):
+        """Set the current model {sequence, range, beam, twiss} dict."""
+        self.mdata = mdata
+        self.w_sequence.SetData(self.session.madx, self.session.utool,
+                                sequence=mdata.get('sequence'),
+                                range=mdata.get('range'))
+        self.OnChangeSequence()
 
     def GetData(self):
-        seq, ran = self.w_sequence.GetData()
+        """Get a model dict with {sequence, range, beam, twiss} fields."""
+        sequence, range = self.w_sequence.GetData()
         return {
-            'sequence': seq,
-            'range': ran,
+            'sequence': sequence,
+            'range': range,
             'beam': self.w_beam.GetData(),
             'twiss': self.w_twiss.GetData(),
         }
 
-    def _UpdateSequence(self):
-        self.w_sequence.SetData(self.session.madx, self.mdata, self.session.utool)
-
     def Validate(self):
+        """Validate the current state of the control."""
         return (self.w_sequence.Validate() and
                 self.w_beam.Validate() and
                 self.w_twiss.Validate())
 
 
 class SequenceRangeWidget(Widget):
+
+    """
+    Allows to select
+
+    - the sequence (among all sequences currently defined in MAD-X)
+    - the range within the selected sequence (via elements defined in MAD-X)
+    """
 
     def CreateControls(self, window):
         """Create sub-controls."""
@@ -95,37 +117,46 @@ class SequenceRangeWidget(Widget):
         self.sequence_picker.Control.Bind(wx.EVT_CHOICE, self.OnChangeSequence)
         return sizer
 
-    def SetData(self, madx, mdata, utool):
+    def SetData(self, madx, utool, sequence, range):
+        """Set the control state."""
         self.madx = madx
-        self.mdata = mdata
         self.utool = utool
+        self.sequence = sequence
+        self.range = range
         self._UpdateSequences()
         self._UpdateRange()
 
     def OnChangeSequence(self):
+        """
+        Act upon changed sequence selection: Update the range and propagate an
+        event upwards.
+        """
         self._UpdateRange()
         ev = wx.PyCommandEvent(wx.EVT_CHOICE.typeId, self.Control.GetId())
         ev.SetString(self.sequence_picker.GetData())
         wx.PostEvent(self.Control.GetEventHandler(), ev)
 
     def _UpdateSequences(self):
+        """Update list of shown sequences."""
         sequences = list(self.madx.sequences)
-        selected_sequence = self.mdata.get('sequence')
+        selected_sequence = self.sequence
         if selected_sequence not in sequences:
             selected_sequence = sequences[0]
         self.sequence_picker.SetData('Sequence:', sequences, selected_sequence)
         self.sequence_picker.ctrl_choices.Bind(wx.EVT_CHOICE, self._UpdateRange)
 
     def _UpdateRange(self, event=None):
+        """Update list of elements in range selection control."""
         seq_name = self.sequence_picker.GetData()
         elements = self.madx.sequences[seq_name].elements
         elements_with_units = map(self.utool.dict_add_unit, elements)
-        if self.mdata.get('range'):
-            beg, end = self.mdata['range']
+        if self.sequence == seq_name and self.range:
+            beg, end = self.range
             selected = [elements.index(beg), elements.index(end)]
         else:
             beg = 0
             end = len(elements) - 1
+            selected = [beg, end]
         self.range_picker.SetData(elements_with_units, selected)
 
     def GetData(self):
