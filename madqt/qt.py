4459bd022f5d247ae9d499083323b57c02ee6902
Thomas Gläßle <t_glaessle@gmx.de>
2016-08-16 11:43:30 +0200

    Prefer PyQt4 over other PyQt5 and PySide

--- madqt/qt.py
+++ madqt/qt.py
@@ -5,4 +5,9 @@ Use this module to get consistent Qt imports.
 """
 
 # this is required for qtconsole (interactive shell) to work:
-from qtconsole.qt import QtCore, QtGui
+from qtconsole.qt_loaders import (load_qt, QT_API_PYSIDE,
+                                  QT_API_PYQT, QT_API_PYQT5)
+
+api_opts = [QT_API_PYQT, QT_API_PYQT5, QT_API_PYSIDE]
+
+QtCore, QtGui, QtSvg, QT_API = load_qt(api_opts)
