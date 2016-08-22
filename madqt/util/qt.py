ef3392556a6e9a6a7bd93b581aa4450d9d353122
Thomas Gläßle <t_glaessle@gmx.de>
2016-08-22 14:12:29 +0200

    Improve documentation for madqt.util.qt

--- madqt/util/qt.py
+++ madqt/util/qt.py
@@ -10,11 +10,19 @@ from madqt.qt import QtCore, QtGui, Qt
 
 
 def notifyCloseEvent(widget, handler):
+    """Connect a closeEvent observer."""
+    # There are three basic ways to get notified when a window is closed:
+    #   - set the WA_DeleteOnClose attribute and connect to the
+    #     QWidget.destroyed signal
+    #   - use installEventFilter / eventFilter
+    #   - hook into the closeEvent method (see notifyEvent below)
+    # We use the first option here since it is the simplest:
     widget.setAttribute(Qt.WA_DeleteOnClose)
     widget.destroyed.connect(handler)
 
 
 def notifyEvent(widget, name, handler):
+    """Connect an event listener."""
     old_handler = getattr(widget, name)
     def new_handler(event):
         handler(event)
