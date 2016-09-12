eab02f95e4ab0d1dbef7514081dd9f7c03f625ad
Thomas Gläßle <t_glaessle@gmx.de>
2016-09-12 11:10:03 +0200

    Drop PySide from the list of supported Qt bindings
    matplotlib supports only PyQt{4,5}

--- madqt/qt.py
+++ madqt/qt.py
@@ -10,7 +10,7 @@ from qtconsole.qt_loaders import load_qt
 
 import os
 
-api_pref = os.environ.get('PYQT_API') or 'pyqt,pyqt5,pyside'
+api_pref = os.environ.get('PYQT_API') or 'pyqt,pyqt5'
 api_opts = api_pref.lower().split(',')
 
 QtCore, QtGui, QtSvg, QT_API = load_qt(api_opts)
