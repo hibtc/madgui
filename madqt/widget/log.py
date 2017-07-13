76c7aedbece388b37a9cb54c61e4717f619a8be7
Thomas Gläßle <t_glaessle@gmx.de>
2017-07-13 20:46:55 +0200

    Basic version of table based log window

--- madqt/widget/log.py
+++ madqt/widget/log.py
@@ -8,26 +8,41 @@ from __future__ import unicode_literals
 
 import logging
 import threading
+import time
+from collections import namedtuple
 
 from six import text_type as unicode
 
 from madqt.qt import Qt, QtCore, QtGui
 from madqt.core.base import Object, Signal
+from madqt.util.collections import List
+from madqt.widget.tableview import ColumnInfo, TableView
 import madqt.util.font as font
 
 
-class LogWindow(QtGui.QPlainTextEdit):
+LogRecord = namedtuple('LogRecord', ['time', 'domain', 'text'])
 
-    def __init__(self, *args):
-        super(LogWindow, self).__init__(*args)
+
+class LogWindow(TableView):
+
+    columns = [
+        ColumnInfo('Time', lambda record: time.strftime(
+            '%H:%M:%S', time.localtime(record.time))),
+        ColumnInfo('Domain', 'domain', resize=QtGui.QHeaderView.ResizeToContents),
+        ColumnInfo('Text', 'text'),
+    ]
+
+    def __init__(self, parent):
+        self.records = List()
+        super(LogWindow, self).__init__(parent, self.columns, self.records)
+        self.horizontalHeader().hide()
         self.setFont(font.monospace())
-        self.setReadOnly(True)
 
     def setup_logging(self, level=logging.INFO,
                       fmt='%(asctime)s %(levelname)s %(name)s: %(message)s',
                       datefmt='%H:%M:%S'):
         # TODO: MAD-X log should be separate from basic logging
-        stream = TextCtrlStream(self)
+        stream = TextCtrlStream(self.records, 'Log')
         root = logging.getLogger('')
         manager = logging.Manager(root)
         formatter = logging.Formatter(fmt, datefmt)
@@ -74,10 +89,11 @@ class TextCtrlStream(object):
     Write to a text control.
     """
 
-    def __init__(self, ctrl):
+    def __init__(self, records, domain):
         """Set text control."""
-        self._ctrl = ctrl
+        self._records = records
+        self._domain = domain
 
     def write(self, text):
         """Append text."""
-        self._ctrl.appendPlainText(text.rstrip())
+        self._records.append(LogRecord(time.time(), self._domain, text))
