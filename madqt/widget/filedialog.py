31851b0320bba87a2de36b7492ae6936b46efe79
Thomas Gläßle <t_glaessle@gmx.de>
2017-11-14 14:10:06 +0100

    Add FileWidget

--- madqt/widget/filedialog.py
+++ madqt/widget/filedialog.py
@@ -4,13 +4,18 @@ Utility functions for use with QFileDialog.
 
 import os
 
-from madqt.qt import QtGui
+from madqt.qt import Qt, QtGui
 
 
 __all__ = [
     'getOpenFileName',
     'getSaveFileName',
+    'FileWidget',
 ]
+
+
+MODE_OPEN = 0
+MODE_SAVE = 1
 
 
 def make_filters(wildcards):
@@ -73,3 +78,49 @@ def getSaveFileName(*args, **kwargs):
     return _fileDialog(QtGui.QFileDialog.AcceptSave,
                        QtGui.QFileDialog.AnyFile,
                        *args, **kwargs)
+
+
+def getFileName(mode, *args, **kwargs):
+    if mode == MODE_OPEN:
+        return getOpenFileName(*args, **kwargs)
+    else:
+        return getSaveFileName(*args, **kwargs)
+
+
+class FileWidget(QtGui.QLineEdit):
+
+    mode = MODE_OPEN
+    title = 'Open file'
+    filters = None
+    filename = None
+    folder = None
+
+    def __init__(self):
+        super().__init__()
+        self.setReadOnly(True)
+        self.setPlaceholderText("filename")
+
+    def keyPressEvent(self, event):
+        if event.key() in (Qt.Key_Enter, Qt.Key_Return, Qt.Key_Space):
+            self.show_open_dialog()
+        super().keyPressEvent(event)
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.show_open_dialog()
+        super().mousePressEvent(event)
+
+    def set_filename(self, filename):
+        if filename is None:
+            self.filename = None
+            self.folder = None
+            self.clear()
+        else:
+            self.filename = filename
+            self.folder = os.path.dirname(os.path.abspath(filename))
+            self.setText(filename)
+
+    def show_open_dialog(self):
+        filename = getFileName(self.mode, self.title, self.folder, self.filters)
+        if filename is not None:
+            self.set_filename(filename)
