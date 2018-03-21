hibtc/madgui

commit 8775a999c06e69e8789e48bb66d1db60cf8ebfe6

Keep values as plain floats internally (in MAD-X units)
Resolves #2

madgui/core/model.py
@@ -11,6 +11,7 @@ import logging
 from bisect import bisect_right
 import subprocess
 from threading import RLock
+from math import isclose
 
 import numpy as np
 
@@ -20,8 +21,6 @@ from cpymad.types import Expression
 
 from madgui.core.base import Object, Signal, Cache
 from madgui.resource import yaml
-from madgui.core.unit import (madx_units, ui_units, from_config, isclose,
-                              number_types)
 from madgui.resource.file import FileResource
 from madgui.util.datastore import DataStore
 
@@ -125,16 +124,13 @@ class Model(Object):
         return ElementInfo(name, element, pos)
 
     def get_beam(self):
-        return madx_units.dict_add_unit(self.get_beam_raw())
+        """Get the beam parameter dictionary."""
+        return self._beam
 
     def set_beam(self, beam):
-        self.set_beam_raw(madx_units.dict_strip_unit(beam))
-
-    def get_twiss_args(self):
-        return madx_units.dict_add_unit(self.get_twiss_args_raw())
-
-    def set_twiss_args(self, twiss):
-        self.set_twiss_args_raw(madx_units.dict_strip_unit(twiss))
+        """Set beam from a parameter dictionary."""
+        self._beam = beam
+        self._use_beam(beam)
 
     def get_globals(self):
         blacklist = ('none', 'twiss_tol', 'degree')
@@ -147,14 +143,12 @@ class Model(Object):
 
     globals = property(get_globals, set_globals)
     beam = property(get_beam, set_beam)
-    twiss_args = property(get_twiss_args, set_twiss_args)
 
     def get_element_by_position(self, pos):
         """Find optics element by longitudinal position."""
         if pos is None:
             return None
-        val = madx_units.strip_unit('s', pos)
-        i0 = bisect_right(self.positions, val)
+        i0 = bisect_right(self.positions, pos)
         return self.elements[i0-1 if i0 > 0 else 0]
 
     def get_element_by_mouse_position(self, axes, pos):
@@ -166,8 +160,7 @@ class Model(Object):
         at, L = elem.At, elem.L
         el_id = elem.El_id
         x0_px = axes.transData.transform_point((0, 0))[0]
-        strip = lambda x: madx_units.strip_unit('at', x)
-        x2pix = lambda x: axes.transData.transform_point((strip(x), 0))[0]-x0_px
+        x2pix = lambda x: axes.transData.transform_point((x, 0))[0]-x0_px
         len_px = x2pix(L)
         pos_px = x2pix(pos)
         if len_px > 5 or elem.Type == 'drift':
@@ -462,7 +455,7 @@ class Model(Object):
         self.continuous_matching = True
 
         self._beam = beam
-        self._twiss_args = twiss_args
+        self.twiss_args = twiss_args
         self._use_beam(beam)
         self.sequence.use()
 
@@ -503,44 +496,23 @@ class Model(Object):
     # TODO…
     def _is_mutable_attribute(self, k, v):
         blacklist = self.config['parameter_sets']['element']['readonly']
-        allowed_types = (list, number_types)
+        allowed_types = (list, int, float, Expression)
         return isinstance(v, allowed_types) and k.lower() not in blacklist
 
-    # TODO: get data from MAD-X
-    def get_twiss_args_raw(self):
-        return self._twiss_args
-
-    def set_twiss_args_raw(self, twiss):
-        self._twiss_args = twiss
-
-    # TODO: get data from MAD-X
-    def get_beam_raw(self):
-        """Get the beam parameter dictionary."""
-        return self._beam
-
-    def set_beam_raw(self, beam):
-        """Set beam from a parameter dictionary."""
-        self._beam = beam
-        self._use_beam(beam)
-
     def update_globals(self, globals):
-        self.set_globals(globals)
+        self.globals = globals
         self.twiss.invalidate()
 
     def update_beam(self, beam):
-        new_beam = self._beam.copy()
-        new_beam.update(
-            (k.lower(), v)
-            for k, v in madx_units.dict_strip_unit(beam).items())
-        self.set_beam_raw(new_beam)
+        new_beam = self.beam.copy()
+        new_beam.update((k.lower(), v) for k, v in beam.items())
+        self.beam = new_beam
         self.twiss.invalidate()
 
     def update_twiss_args(self, twiss):
-        new_twiss = self._twiss_args
-        new_twiss.update(
-            (k.lower(), v)
-            for k, v in madx_units.dict_strip_unit(twiss).items())
-        self.set_twiss_args_raw(new_twiss)
+        new_twiss = self.twiss_args.copy()
+        new_twiss.update((k.lower(), v) for k, v in twiss.items())
+        self.twiss_args = new_twiss
         self.twiss.invalidate()
 
     def update_element(self, data, elem_index):
@@ -552,7 +524,6 @@ class Model(Object):
         d = {k.lower(): v for k, v in data.items()
              if self._is_mutable_attribute(k, v)
              and elem[k.lower()] != v}
-        d = madx_units.dict_strip_unit(d)
         if any(isinstance(v, (list,str)) for v in d.values()):
             self.madx.command(name, **d)
         else:
@@ -581,7 +552,7 @@ class Model(Object):
         x = self.indices[ix]
 
         # shortcut for thin elements:
-        if madx_units.strip_unit('l', self.elements[ix].L) == 0:
+        if float(self.elements[ix].L) == 0:
             return y[x]
 
         lo = x.start-1 if x.start > 0 else x.start
@@ -626,12 +597,11 @@ class Model(Object):
                 self.stop.index >= element.index)
 
     def _get_twiss_args(self, **kwargs):
-        twiss_init = madx_units.dict_strip_unit(self.twiss_args)
         twiss_args = {
             'sequence': self.sequence.name,
             'range': self.range,
             'columns': self._columns,
-            'twiss_init': twiss_init,
+            'twiss_init': self.twiss_args,
         }
         twiss_args.update(kwargs)
         return twiss_args
@@ -690,7 +660,7 @@ class Model(Object):
                                  col('sig12') * col('sig21'))**0.5
         if name == 'ey': return (col('sig33') * col('sig44') -
                                  col('sig34') * col('sig43'))**0.5
-        return madx_units.add_unit(name, self.twiss.data[name])
+        return self.twiss.data[name]
 
     def get_twiss_column(self, column):
         if column not in self.cache:
@@ -734,7 +704,7 @@ class Model(Object):
         self.madx.command.select(flag='interpolate', clear=True)
         self.madx.command.select(flag='interpolate', step=step)
         results = self.madx.twiss(**self._get_twiss_args())
-        self.summary = madx_units.dict_add_unit(results.summary)
+        self.summary = results.summary
 
         # FIXME: this will fail if subsequent element have the same name.
         # Safer alternatives:
@@ -779,22 +749,21 @@ class Model(Object):
         madx_constraints = [
             {'range': elem.Name,
              'iindex': elem_positions[elem.Name].index(pos),
-             axis: madx_units.strip_unit(axis, val)}
+             axis: val}
             for elem, pos, axis, val in constraints]
 
         # FIXME TODO: use position-dependent emittances…
-        ex = madx_units.strip_unit('ex', self.ex())
-        ey = madx_units.strip_unit('ey', self.ey())
+        ex = self.ex()
+        ey = self.ey()
         weights = {
             'sig11': 1/ex, 'sig12': 1/ex, 'sig21': 1/ex, 'sig22': 1/ex,
             'sig33': 1/ey, 'sig34': 1/ey, 'sig43': 1/ey, 'sig44': 1/ey,
         }
-        twiss_args = madx_units.dict_strip_unit(self.twiss_args)
         self.madx.match(sequence=self.sequence.name,
                         vary=variables,
                         constraints=madx_constraints,
                         weight=weights,
-                        twiss_init=twiss_args)
+                        twiss_init=self.twiss_args)
         # TODO: update only modified elements
         self.elements.invalidate()
         self.twiss.invalidate()
@@ -822,16 +791,14 @@ class Model(Object):
         except IndexError:
             return
         if expr is not None:
-            return api.Knob(
-                self, elem, attr, expr,
-                madx_units._units.get(attr), vars)
+            return api.Knob(self, elem, attr, expr, None, vars)
 
     def read_param(self, expr):
-        """Read element attribute. Return numeric value. No units!"""
+        """Read element attribute. Return numeric value."""
         return self.madx.evaluate(expr)
 
     def write_param(self, expr, value):
-        """Update element attribute into control system. No units!"""
+        """Update element attribute into control system."""
         self.madx.set_value(expr, value)
         self.twiss.invalidate()
         # TODO: invalidate element…
@@ -1049,7 +1016,7 @@ class Element(Mapping):
             index = int(tail[:-1])
             return self._get_field(head, index)
         self._retrieve(name)
-        return madx_units.add_unit(name, self._merged[name])
+        return self._merged[name]
 
     def __iter__(self):
         self._retrieve(None)

madgui/core/unit.py
@@ -78,6 +78,8 @@ def strip_unit(quantity, unit=None):
         # FIXME: 'zip' truncates without warning if not enough units
         # are defined
         return [q.to(u).magnitude for q, u in zip(quantity, unit)]
+    if isinstance(quantity, Expression):
+        return float(quantity)
     try:
         return quantity.to(unit).magnitude
     except AttributeError:
@@ -197,7 +199,7 @@ class UnitConverter:
 
     def add_unit(self, name, value):
         """Add units to a single number."""
-        unit = self._units.get(name)
+        unit = self._units.get(name) if isinstance(name, str) else name
         if unit:
             if isinstance(value, (list, tuple)):
                 # FIXME: 'zip' truncates without warning if not enough units
@@ -222,7 +224,8 @@ class UnitConverter:
 
     def strip_unit(self, name, value):
         """Convert to MAD-X units."""
-        return strip_unit(value, self._units.get(name))
+        unit = self._units.get(name) if isinstance(name, str) else name
+        return strip_unit(value, unit)
 
     def dict_add_unit(self, obj):
         """Add units to all elements in a dictionary."""
@@ -250,3 +253,19 @@ madx_units = UnitConverter.from_config_dict(yaml.safe_load(
 
 ui_units = UnitConverter.from_config_dict(yaml.safe_load(
     resource_string('madgui.data', 'ui_units.yml')))
+
+
+def convert(from_, to, *args):
+    if len(args) == 1:
+        return to.dict_strip_unit(from_.dict_add_unit(*args))
+    if len(args) == 2:
+        return to.strip_unit(args[0], from_.add_unit(*args))
+    if len(args) == 3:
+        return to.strip_unit(args[0], from_.add_unit(*args[1:]))
+    raise TypeError("convert can only be called with one or two arguments.")
+
+def from_ui(*args):
+    return convert(ui_units, madx_units, *args)
+
+def to_ui(*args):
+    return convert(madx_units, ui_units, *args)

madgui/correct/multi_grid.py
@@ -15,7 +15,7 @@ import yaml
 
 from madgui.qt import QtCore, QtGui, uic
 
-from madgui.core.unit import tounit, madx_units
+from madgui.core.unit import tounit
 from madgui.util.collections import List
 from madgui.util.layout import VBoxLayout
 from madgui.util.qt import fit_button
@@ -71,8 +71,7 @@ class Corrector(Matcher):
         self.monitors[:] = monitors
         elements = self.model.elements
         self.constraints[:] = sorted([
-            Constraint(elements[target], elements[target].At, key,
-                       madx_units.add_unit(key, value))
+            Constraint(elements[target], elements[target].At, key, value)
             for target, values in targets.items()
             for key, value in values.items()
             if key[-1] in dirs
@@ -133,14 +132,13 @@ class Corrector(Matcher):
         secmaps = list(itertools.accumulate(secmaps, lambda a, b: np.dot(b, a)))
 
         (x, px, y, py), chi_squared, singular = fit_initial_orbit(*[
-            (secmap[:,:6], secmap[:,6], (madx_units.strip_unit('x', record.x),
-                                         madx_units.strip_unit('y', record.y)))
+            (secmap[:,:6], secmap[:,6], (record.x, record.y))
             for record, secmap in zip(records, secmaps)
         ])
-        return madx_units.dict_add_unit({
+        return {
             'x': x, 'px': px,
             'y': y, 'py': py,
-        }), chi_squared, singular
+        }, chi_squared, singular
 
     def compute_steerer_corrections(self, init_orbit):
 
@@ -162,9 +160,7 @@ class Corrector(Matcher):
                        for var in v.knob.vars
                        if var.lower() not in blacklist}
         constraints = [
-            dict(range=c.elem.Name, **madx_units.dict_strip_unit({
-                c.axis: c.value
-            }))
+            dict(range=c.elem.Name, **{c.axis: c.value})
             for c in self.constraints
         ]
         self.model.madx.command.select(flag='interpolate', clear=True)
@@ -174,7 +170,7 @@ class Corrector(Matcher):
             method=('jacobian', {}),
             weight={'x': 1e3, 'y':1e3, 'px':1e2, 'py':1e2},
             constraints=constraints,
-            twiss_init=madx_units.dict_strip_unit(init_twiss))
+            twiss_init=init_twiss)
         self.model.twiss.invalidate()
 
         # return corrections

madgui/correct/optic_variation.py
@@ -6,7 +6,7 @@ alignment.
 from pkg_resources import resource_filename
 
 from madgui.qt import Qt, QtCore, QtGui, uic
-from madgui.core.unit import get_unit, allclose, tounit, madx_units
+from madgui.core.unit import get_unit, allclose, tounit
 from madgui.widget.tableview import ColumnInfo
 from madgui.util.collections import List
 from madgui.util.qt import notifyEvent
@@ -184,10 +184,10 @@ class Corrector:
         ])
         initial_orbit, chi_squared, singular = self.fit_results
         x, px, y, py = initial_orbit
-        return madx_units.dict_add_unit({
+        return {
             'x': x, 'px': px,
             'y': y, 'py': py,
-        }), chi_squared, singular
+        }, chi_squared, singular
 
     def compute_steerer_corrections(self, init_orbit, design_orbit,
                                     correct_x=None, correct_y=None,
@@ -226,7 +226,7 @@ class Corrector:
         # match final conditions
         match_names = [mknob.param for mknob, _ in steerer_knobs]
         constraints = [
-            dict(range=target, **madx_units.dict_strip_unit(orbit))
+            dict(range=target, **orbit)
             for target, orbit in zip(self.targets, design_orbit)
         ]
         self.model.madx.match(
@@ -234,7 +234,7 @@ class Corrector:
             vary=match_names,
             weight={'x': 1e3, 'y':1e3, 'px':1e3, 'py':1e3},
             constraints=constraints,
-            twiss_init=madx_units.dict_strip_unit(init_twiss))
+            twiss_init=init_twiss)
         self.model.twiss.invalidate()
 
         # return corrections
@@ -254,9 +254,8 @@ class Corrector:
         self.model.twiss_args = self.backup_twiss_args
 
     def _strip_sd_pair(self, sd_values, prefix='pos'):
-        strip_unit = madx_units.strip_unit
-        return (strip_unit('x', sd_values[prefix + 'x']),
-                strip_unit('y', sd_values[prefix + 'y']))
+        return ('x', sd_values[prefix + 'x'],
+                'y', sd_values[prefix + 'y'])
 
 
 def _is_steerer(el):

madgui/data/config.yml
@@ -19,9 +19,6 @@ mainwindow:
 # subcomponents):
 line_view:
 
-  x_label: s
-  x_unit: m
-
   # Plot style for the element indicators. Supports common keyword arguments
   # for ``matplotlib.axes.Axes.axvspan`` and ``matplotlib.axes.Axes.axvline``:
   element_style:

madgui/online/api.py
@@ -23,8 +23,9 @@ The interface contract is currently designed as follows:
 """
 
 from abc import ABCMeta, abstractmethod
+from math import pi
 
-from madgui.core.unit import add_unit, strip_unit, units
+from madgui.core.unit import from_ui, to_ui
 
 _Interface = ABCMeta('_Interface', (object,), {})
 
@@ -81,11 +82,11 @@ class OnlinePlugin(_Interface):
 
     @abstractmethod
     def read_param(self, param):
-        """Read parameter. Return numeric value. No units!"""
+        """Read parameter. Return numeric value."""
 
     @abstractmethod
     def write_param(self, param, value):
-        """Update parameter into control system. No units!"""
+        """Update parameter into control system."""
 
     @abstractmethod
     def get_beam(self):
@@ -114,15 +115,15 @@ class Knob:
     def __repr__(self):
         return "{}({})".format(self.__class__.__name__, self)
 
-    # mixins for units and conversion:
+    # mixins:
 
     def read(self):
-        """Read element attribute. Return numeric value including unit."""
-        return add_unit(self.plug.read_param(self.param), self.unit)
+        """Read element attribute."""
+        return from_ui(self.attr, self.unit, self.plug.read_param(self.param))
 
     def write(self, value):
         """Update element attribute into control system."""
-        self.plug.write_param(self.param, strip_unit(value, self.unit))
+        self.plug.write_param(self.param, to_ui(self.unit, self.attr, value))
 
     def to(self, attr, value):
         try:
@@ -137,6 +138,6 @@ CONVERTERS = {
     ('kl', 'k1'): lambda knob, val: val / knob.elem.L,
     ('k1s', 'kl'): lambda knob, val: val * knob.elem.L,
     ('kl', 'k1s'): lambda knob, val: val / knob.elem.L,
-    ('angle', 'gantry'): lambda knob, val: -val + 90*units.degree,
-    ('gantry', 'angle'): lambda knob, val: -val + 90*units.degree,
+    ('angle', 'gantry'): lambda knob, val: -val + pi/2,
+    ('gantry', 'angle'): lambda knob, val: -val + pi/2,
 }

madgui/online/control.py
@@ -150,7 +150,7 @@ class Control(Object):
         ]
         rows = [
             SyncParamItem(self._plugin.param_info(dknob),
-                          dval, mknob.to(dknob.attr, mval))
+                          dval, mknob.to(dknob.attr, mval), dknob.attr)
             for mknob, mval, dknob, dval in knobs
         ]
         if not rows:

madgui/online/dialogs.py
@@ -3,12 +3,9 @@ Dialog for selecting DVM parameters to be synchronized.
 """
 
 from madgui.qt import QtGui
-from madgui.core.unit import strip_unit, get_raw_label, ui_units
+from madgui.core.unit import to_ui, ui_units
 from madgui.util.layout import VBoxLayout
 from madgui.widget.tableview import TableView, ColumnInfo
-
-
-# TODO: use UI units
 
 
 class ListSelectWidget(QtGui.QWidget):
@@ -45,13 +42,12 @@ class ListSelectWidget(QtGui.QWidget):
 
 class SyncParamItem:
 
-    def __init__(self, param, dvm_value, mad_value):
+    def __init__(self, param, dvm_value, mad_value, attr):
         self.param = param
         self.name = param.name
-        # TODO: simply use ui_units, remove param.ui_unit
-        self.unit = get_raw_label(param.ui_unit)
-        self.dvm_value = strip_unit(dvm_value, param.ui_unit)
-        self.mad_value = strip_unit(mad_value, param.ui_unit)
+        self.unit = ui_units.label(attr)
+        self.dvm_value = to_ui(attr, dvm_value)
+        self.mad_value = to_ui(attr, mad_value)
 
 
 class SyncParamWidget(ListSelectWidget):
@@ -89,10 +85,10 @@ class MonitorItem:
 
     def __init__(self, el_name, values):
         self.name = el_name
-        self.posx = ui_units.strip_unit('x', values.get('posx'))
-        self.posy = ui_units.strip_unit('x', values.get('posy'))
-        self.envx = ui_units.strip_unit('x', values.get('envx'))
-        self.envy = ui_units.strip_unit('x', values.get('envy'))
+        self.posx = to_ui('x', values.get('posx'))
+        self.posy = to_ui('x', values.get('posy'))
+        self.envx = to_ui('x', values.get('envx'))
+        self.envy = to_ui('x', values.get('envy'))
         self.unit = ui_units.label('x')
 
 

commit fe20fe765041ff8bb9176372ee1521113e3d4016

Replace all Expression instances by their values
This fixes a bug when showing expressions in TableView: The widget would
display only the expression, not its value -> leading to an inconsistent
look. Furthemore, when trying to edit the value in the TableView, there
was an error due to QuantitySpinBox being broken for Expression.

This problem was introduced in

    8775a999c06e69e8789e48bb66d1db60cf8ebfe6
    "Keep values as plain floats internally (in MAD-X units)"

i.e. when we started to use un-qualified values rather than unit-aware
quantities internally. Before this, all expressions were converted to
SymbolicValue which worked correctly with TableView.

madgui/core/model.py
@@ -133,7 +133,8 @@ class Model(Object):
 
     def get_globals(self):
         blacklist = ('none', 'twiss_tol', 'degree')
-        return {k: v for k, v in self.madx.globals.items()
+        return {k: _eval_expr(v)
+                for k, v in self.madx.globals.items()
                 if k not in blacklist}
 
     def set_globals(self, knobs):
@@ -388,8 +389,8 @@ class Model(Object):
         return {
             'sequence': sequence_name,
             'range': range,
-            'beam': beam,
-            'twiss': twiss,
+            'beam': _eval_expr(beam),
+            'twiss': _eval_expr(twiss),
         }
 
     def _get_twiss(self, sequence):
@@ -1055,6 +1056,7 @@ class Element(Mapping):
         """Retrieve data for key if possible; everything if None."""
         if len(self._merged) == 2 and name not in self._merged:
             data = self._model.active_sequence.expanded_elements[self._idx]
+            data = _eval_expr(data)
             self._merged.update(data)
 
 
@@ -1118,3 +1120,15 @@ def _is_property_defined(elem, attr):
         return hasattr(value, '_expression') or float(value) != 0
     except (ValueError, TypeError, IndexError):
         return False
+
+
+def _eval_expr(value):
+    """Helper method that replaces :class:`Expression` by their values."""
+    # NOTE: This method will become unnecessary in cpymad 1.0.
+    if isinstance(value, Expression):
+        return value.value
+    if isinstance(value, list):
+        return [_eval_expr(v) for v in value]
+    if isinstance(value, dict):
+        return {k: _eval_expr(v) for k, v in value.items()}
+    return value

madgui/widget/tableview.py
@@ -6,7 +6,6 @@ from inspect import getmro
 
 from madgui.qt import QtCore, QtGui, Qt
 from madgui.core.base import Object, Signal
-from madgui.core.unit import Expression
 from madgui.core.config import NumberFormat
 from madgui.util.layout import HBoxLayout
 from madgui.util.misc import rw_property
@@ -588,7 +587,6 @@ defaultTypes.update({
     bytes: StringValue,
     list: ListValue,                        # TODO: VECTOR vs MATRIX…
     unit.units.Quantity: QuantityValue,
-    Expression: QuantityValue,
     Enum: EnumValue,
 })
 