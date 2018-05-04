05aeabe19d54b28e9d655d3a5c8092413dbdf8f9
Thomas Gläßle <t_glaessle@gmx.de>
2018-05-04 13:32:46 +0200

    Merge emittance into diagnostic module

--- madgui/online/diagnostic.py
+++ madgui/online/diagnostic.py
@@ -1,4 +1,8 @@
 import os
+from math import sqrt, isnan
+from collections import namedtuple
+from itertools import accumulate
+import logging
 
 import numpy as np
 
@@ -8,7 +12,6 @@ from madgui.util import yaml
 from madgui.util.layout import VBoxLayout
 from madgui.util.collections import List
 from madgui.widget.tableview import ColumnInfo, ExtColumnInfo
-from madgui.online.emittance import EmittanceDialog
 
 
 class MonitorWidget(QtGui.QDialog):
@@ -18,7 +21,7 @@ class MonitorWidget(QtGui.QDialog):
         self.tabs = QtGui.QTabWidget()
         self.tabs.addTab(PlotMonitorWidget(control, model, frame), "Plot")
         self.tabs.addTab(OrbitWidget(control, model, frame), "Orbit")
-        self.tabs.addTab(EmittanceDialog(control), "Optics")
+        self.tabs.addTab(EmittanceDialog(control, model, frame), "Optics")
         self.setLayout(VBoxLayout([self.tabs], tight=True))
         self.setSizeGripEnabled(True)
 
@@ -37,6 +40,9 @@ class MonitorItem:
                      not np.isclose(self.posy, -9.999))
 
 
+ResultItem = namedtuple('ResultItem', ['name', 'fit', 'model'])
+
+
 def get_monitor_name(mgr, monitor, i):
     return monitor.name
 
@@ -51,6 +57,10 @@ def set_monitor_show(cell, show):
         mgr.select(i)
     elif not show and shown:
         mgr.deselect(i)
+
+
+def get_monitor_valid(cell):
+    return cell.item.show
 
 
 class MonitorWidgetBase(QtGui.QWidget):
@@ -239,16 +249,9 @@ class PlotMonitorWidget(MonitorWidgetBase):
             .format(ext))
 
 
-class OrbitWidget(MonitorWidgetBase):
+class _FitWidget(MonitorWidgetBase):
 
     ui_file = 'emittance.ui'
-
-    monitor_columns = [
-        ExtColumnInfo("Monitor", get_monitor_name, checkable=True,
-                      checked=get_monitor_show, setChecked=set_monitor_show),
-        ColumnInfo("x", 'posx', convert=True),
-        ColumnInfo("y", 'posy', convert=True),
-    ]
 
     result_columns = [
         ColumnInfo("Name", 'name', resize=QtGui.QHeaderView.Stretch),
@@ -263,13 +266,26 @@ class OrbitWidget(MonitorWidgetBase):
         Buttons = QtGui.QDialogButtonBox
         self.std_buttons.button(Buttons.Apply).clicked.connect(self.apply)
         #self.btn_offsets.clicked.connect(self.save_offsets)
-        self.options_box.hide()
         self.results = List()
 
         self.rtab.horizontalHeader().setHighlightSections(False)
         self.rtab.setSelectionBehavior(QtGui.QAbstractItemView.SelectRows)
         self.rtab.setSelectionMode(QtGui.QAbstractItemView.ExtendedSelection)
         self.rtab.set_columns(self.result_columns, self.results, self)
+
+
+class OrbitWidget(_FitWidget):
+
+    monitor_columns = [
+        ExtColumnInfo("Monitor", get_monitor_name, checkable=True,
+                      checked=get_monitor_show, setChecked=set_monitor_show),
+        ColumnInfo("x", 'posx', convert=True),
+        ColumnInfo("y", 'posy', convert=True),
+    ]
+
+    def __init__(self, control, model, frame):
+        super().__init__(control, model, frame)
+        self.options_box.hide()
 
     def showEvent(self, event):
         self.frame.open_graph('orbit')
@@ -290,7 +306,6 @@ class OrbitWidget(MonitorWidgetBase):
             self.model.twiss.invalidate()
 
     def on_update(self):
-        from madgui.online.emittance import ResultItem
         self.init_orbit, chi_squared, self.singular = \
             self.fit_particle_orbit()
         if not self.singular:
@@ -302,12 +317,11 @@ class OrbitWidget(MonitorWidgetBase):
 
     def fit_particle_orbit(self):
         from madgui.correct.orbit import fit_initial_orbit
-        import itertools
 
         records = [m for m in self.monitors if self.selected(m)]
         self.restore()
         secmaps = self.model.get_transfer_maps([r.name for r in records])
-        secmaps = list(itertools.accumulate(secmaps, lambda a, b: np.dot(b, a)))
+        secmaps = list(accumulate(secmaps, lambda a, b: np.dot(b, a)))
         (x, px, y, py), chi_squared, singular = fit_initial_orbit(*[
             (secmap[:,:6], secmap[:,6], (record.posx+dx, record.posy+dy))
             for record, secmap in zip(records, secmaps)
@@ -317,3 +331,200 @@ class OrbitWidget(MonitorWidgetBase):
             'x': x, 'px': px,
             'y': y, 'py': py,
         }, chi_squared, singular
+
+
+class EmittanceDialog(_FitWidget):
+
+    monitor_columns = [
+        ColumnInfo("Monitor", 'name', checkable=get_monitor_valid,
+                   checked=get_monitor_show,
+                   setChecked=set_monitor_show,
+                   resize=QtGui.QHeaderView.Stretch),
+        ColumnInfo("Δx", 'envx', convert=True),
+        ColumnInfo("Δy", 'envy', convert=True),
+    ]
+
+    # The three steps of UI initialization
+
+    def __init__(self, control, model, frame):
+        super().__init__(control, model, frame)
+        Buttons = QtGui.QDialogButtonBox
+        self.std_buttons.button(Buttons.Save).clicked.connect(self.export)
+        self.long_transfer.clicked.connect(self.match_values)
+        self.use_dispersion.clicked.connect(self.match_values)
+        self.respect_coupling.clicked.connect(self.match_values)
+
+    def showEvent(self, event):
+        self.frame.open_graph('envelope')
+        self.update()
+
+    def apply(self):
+        results = {r.name: r.fit
+                   for r in self.results
+                   if not isnan(r.fit)}
+        if results:
+            model = self.model
+            model.update_beam({
+                'ex': results.pop('ex', model.ex()),
+                'ey': results.pop('ey', model.ey()),
+            })
+            model.twiss_args.update(results)
+            model.twiss.invalidate()
+
+    def export(self):
+        pass
+
+    def on_update(self):
+        self.cached_tms = None
+        self.match_values()
+
+    def match_values(self):
+
+        long_transfer = self.long_transfer.isChecked()
+        use_dispersion = self.use_dispersion.isChecked()
+        respect_coupling = self.respect_coupling.isChecked()
+
+        min_monitors = 6 if use_dispersion else 3
+        if len(self._selected) < min_monitors:
+            self.results[:] = []
+            return
+
+        model = self.control._model
+
+        monitors = [m for m in self.monitors if self.selected(m)]
+        monitors = sorted(monitors, key=lambda m: model.elements.index(m.name))
+
+        # second case can happen when `removing` a monitor
+        if self.cached_tms is None or len(self.cached_tms) != len(monitors):
+            self.cached_tms = model.get_transfer_maps([m.name for m in monitors])
+
+        tms = list(self.cached_tms)
+        if not long_transfer:
+            tms[0] = np.eye(7)
+        tms = list(accumulate(tms, lambda a, b: np.dot(b, a)))
+        # keep X,PX,Y,PY,PT:
+        tms = np.array(tms)[:,[0,1,2,3,5],:][:,:,[0,1,2,3,5]]
+
+        # TODO: button for "resync model"
+
+        # TODO: when 'interpolate' is on -> choose correct element...?
+        # -> not important for l=0 monitors
+
+        coup_xy = not np.allclose(tms[:,0:2,2:4], 0)
+        coup_yx = not np.allclose(tms[:,2:4,0:2], 0)
+        coup_xt = not np.allclose(tms[:,0:2,4:5], 0)
+        coup_yt = not np.allclose(tms[:,2:4,4:5], 0)
+
+        coupled = coup_xy or coup_yx
+        dispersive = coup_xt or coup_yt
+
+        envx = [m.envx for m in monitors]
+        envy = [m.envy for m in monitors]
+        xcs = [[(0, cx**2), (2, cy**2)]
+               for cx, cy in zip(envx, envy)]
+
+        # TODO: do we need to add dpt*D to sig11 in online control?
+
+        def calc_sigma(tms, xcs, dispersive):
+            if dispersive and not use_dispersion:
+                logging.warn("Dispersive lattice!")
+            if not use_dispersion:
+                tms = tms[:,:-1,:-1]
+            sigma, residuals, singular = solve_emit_sys(tms, xcs)
+            return sigma
+
+        # TODO: assert no dispersion / or use 6 monitors...
+        if not respect_coupling:
+            if coupled:
+                logging.warn("Coupled lattice!")
+            tmx = np.delete(np.delete(tms, [2,3], axis=1), [2,3], axis=2)
+            tmy = np.delete(np.delete(tms, [0,1], axis=1), [0,1], axis=2)
+            xcx = [[(0, cx[1])] for cx, cy in xcs]
+            xcy = [[(0, cy[1])] for cx, cy in xcs]
+            sigmax = calc_sigma(tmx, xcx, coup_xt)
+            sigmay = calc_sigma(tmy, xcy, coup_yt)
+            ex, betx, alfx = twiss_from_sigma(sigmax[0:2,0:2])
+            ey, bety, alfy = twiss_from_sigma(sigmay[0:2,0:2])
+            pt = sigmax[-1,-1]
+
+        else:
+            sigma = calc_sigma(tms, xcs, dispersive)
+            ex, betx, alfx = twiss_from_sigma(sigma[0:2,0:2])
+            ey, bety, alfy = twiss_from_sigma(sigma[2:4,2:4])
+            pt = sigma[-1,-1]
+
+        beam = model.sequence.beam
+        twiss_args = model.twiss_args
+
+        results = []
+        results += [
+            ResultItem('ex',   ex,   beam.ex),
+            ResultItem('ey',   ey,   beam.ey),
+        ]
+        results += [
+            ResultItem('pt',   pt,   beam.et),
+        ] if use_dispersion else []
+        results += [
+            ResultItem('betx', betx, twiss_args.get('betx')),
+            ResultItem('bety', bety, twiss_args.get('bety')),
+            ResultItem('alfx', alfx, twiss_args.get('alfx')),
+            ResultItem('alfy', alfy, twiss_args.get('alfy')),
+        ] if long_transfer else []
+
+        self.results[:] = results
+
+
+def solve_emit_sys(Ms, XCs):
+    """
+    Solve for S the linear system of equations:
+
+        (M S Mᵀ)ₓₓ = C
+
+    For some M, x and C.
+
+    M can be coupled, but S is assumed to be block diagonal, i.e. decoupled:
+
+        S = (X 0 0
+             0 Y 0
+             0 0 T)
+
+    Returns S as numpy array.
+    """
+    d = Ms[0].shape[0]
+
+    con_func = lambda u: [
+        M[[x]].dot(u).dot(M[[x]].T).sum()   # linear beam transport!
+        for M, xc in zip(Ms, XCs)           # for every given transfer matrix
+        for x, _ in xc                      # and measured constraint
+    ]
+
+    sq_matrix_basis = np.eye(d*d,d*d).reshape((d*d,d,d))
+    is_upper_triang = [i for i, m in enumerate(sq_matrix_basis)
+                       if np.allclose(np.triu(m), m)
+                       and (d < 4 or np.allclose(m[0:2,2:4], 0))]
+
+    lhs = np.vstack([
+        con_func(2*u-np.tril(u))
+        for u in sq_matrix_basis[is_upper_triang]
+    ]).T
+    rhs = [c for xc in XCs for _, c in xc]
+
+    x0, residuals, rank, singular = np.linalg.lstsq(lhs, rhs, rcond=-1)
+
+    res = np.tensordot(x0, sq_matrix_basis[is_upper_triang], 1)
+    res = res + res.T - np.tril(res)
+    return res, sum(residuals), (rank<len(x0))
+
+
+def twiss_from_sigma(sigma):
+    """Compute 1D twiss parameters from 2x2 sigma matrix."""
+    b = sigma[0,0]
+    a = sigma[0,1]  # = sigma[1,0] !
+    c = sigma[1,1]
+    if b*c <= a*a:
+        nan = float("nan")
+        return nan, nan, nan
+    emit = sqrt(b*c - a*a)
+    beta = b/emit
+    alfa = a/emit * (-1)
+    return emit, beta, alfa
