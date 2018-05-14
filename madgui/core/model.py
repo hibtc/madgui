3772b834b4c4372291fd75be71f415c072107ab0
Thomas Gläßle <t_glaessle@gmx.de>
2018-05-14 13:18:21 +0200

    Compare expressions without spaces

--- madgui/core/model.py
+++ madgui/core/model.py
@@ -1048,13 +1048,20 @@ def _eval_expr(value):
 def items(d):
     return d.items() if isinstance(d, Mapping) else d
 
+def trim(s):
+    return s.replace(' ', '') if isinstance(s, str) else s
+
+
 class UpdateCommand(QtGui.QUndoCommand):
 
     def __init__(self, old, new, write, text):
         super().__init__()
         old = {k.lower(): v for k, v in items(old)}
         new = {k.lower(): v for k, v in items(new)}
-        self._new = {k: v for k, v in items(new) if old.get(k) != v}
+        # NOTE: This trims not only expressions (as intended) but also regular
+        # string arguments (which is incorrect). However, this should be a
+        # sufficiently rare use case, so we don't care for now…
+        self._new = {k: v for k, v in items(new) if trim(old.get(k)) != trim(v)}
         self._old = {k: v for k, v in items(old) if k in self._new}
         self._old.update({k: None for k in self._new.keys() - self._old.keys()})
         self._set = write
