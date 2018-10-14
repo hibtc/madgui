f1073fa27ab215ea5def80c402d699f5ef961efc
Thomas Gläßle <t_glaessle@gmx.de>
2018-10-14 03:51:33 +0200

    Inject session as fixture
    Decrease indentation:)

--- tests/test_corrector.py
+++ tests/test_corrector.py
@@ -24,19 +24,8 @@ def app():
     return app
 
 
-def test_simple_procedure(app):
-    config = load_config(isolated=True)
-    with Session(config) as session:
-        session.load_model(
-            session.find_model('hit_models/hht3'))
-        session.control.set_backend('hit_csys.plugin:TestBackend')
-        session.control.connect()
-
-        corrector = Corrector(session)
-        assert corrector.fit_results is None
-
-
-def test_procbot(app):
+@pytest.yield_fixture
+def session(app):
     config = load_config(isolated=True)
     with Session(config) as session:
         session.control._settings.update({
@@ -45,48 +34,62 @@ def test_procbot(app):
             'auto_params': True,
             'auto_sd': True,
         })
-        session.load_model(
-            session.find_model('hit_models/hht3'))
-        session.control.set_backend('hit_csys.plugin:TestBackend')
-        session.control.connect()
+        yield session
 
-        corrector = Corrector(session)
-        corrector.setup({
-            'monitors': [
-                't3dg2g',
-                't3dg1g',
-                't3df1',
-            ],
-            'optics':   [
-                'ax_g3mw2',
-                'ax_g3ms2',
-                'ay_g3mw1',
-                'ay_g3ms1',
-            ],
-        })
-        num_mons = 3
-        num_optics = 5          # 4+1 for base_optics
-        num_shots = 2
 
-        corrector.set_optics_delta({}, 1e-4)
-        corrector.open_export('timeseries.yml')
-        assert os.path.exists('timeseries.yml')
+def test_simple_procedure(session):
+    session.load_model(
+        session.find_model('hit_models/hht3'))
+    session.control.set_backend('hit_csys.plugin:TestBackend')
+    session.control.connect()
 
-        try:
-            widget = mock.Mock()
-            procbot = ProcBot(widget, corrector)
-            procbot.start(1, num_shots, gui=False)
+    corrector = Corrector(session)
+    assert corrector.fit_results is None
 
-            assert widget.update_ui.call_count == 1
 
-            i = 0
-            while procbot.running and i < 100:
-                procbot.poll()
-                time.sleep(0.010)
-                i += 1
+def test_procbot(session):
+    session.load_model(
+        session.find_model('hit_models/hht3'))
+    session.control.set_backend('hit_csys.plugin:TestBackend')
+    session.control.connect()
 
-            assert not procbot.running
-            assert len(corrector.records) == num_mons * num_optics * num_shots
+    corrector = Corrector(session)
+    corrector.setup({
+        'monitors': [
+            't3dg2g',
+            't3dg1g',
+            't3df1',
+        ],
+        'optics':   [
+            'ax_g3mw2',
+            'ax_g3ms2',
+            'ay_g3mw1',
+            'ay_g3ms1',
+        ],
+    })
+    num_mons = 3
+    num_optics = 5          # 4+1 for base_optics
+    num_shots = 2
 
-        finally:
-            os.remove('timeseries.yml')
+    corrector.set_optics_delta({}, 1e-4)
+    corrector.open_export('timeseries.yml')
+    assert os.path.exists('timeseries.yml')
+
+    try:
+        widget = mock.Mock()
+        procbot = ProcBot(widget, corrector)
+        procbot.start(1, num_shots, gui=False)
+
+        assert widget.update_ui.call_count == 1
+
+        i = 0
+        while procbot.running and i < 100:
+            procbot.poll()
+            time.sleep(0.010)
+            i += 1
+
+        assert not procbot.running
+        assert len(corrector.records) == num_mons * num_optics * num_shots
+
+    finally:
+        os.remove('timeseries.yml')
