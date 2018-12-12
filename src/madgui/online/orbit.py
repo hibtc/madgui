1c4c64d3ed1bc5b0d5d2a6ae9dff799df2deb9ab
Thomas Gläßle <t_glaessle@gmx.de>
2018-12-12 15:11:24 +0100

    Allow more general positions

--- src/madgui/online/orbit.py
+++ src/madgui/online/orbit.py
@@ -16,7 +16,7 @@ def add_offsets(readouts, offsets):
     ] if offsets else readouts
 
 
-def fit_particle_readouts(model, readouts, backward=True):
+def fit_particle_readouts(model, readouts, to='#s'):
     index = model.elements.index
     readouts = [
         r if hasattr(r, 'name') else Readout(*r)
@@ -24,14 +24,13 @@ def fit_particle_readouts(model, readouts, backward=True):
     ]
     readouts = sorted(readouts, key=lambda r: index(r.name))
     range_start = readouts[0].name
-    fit_method = fit_particle_orbit if backward else fit_forward_orbit
-    return fit_method(model, readouts, [
+    return fit_particle_orbit(model, readouts, [
         model.sectormap(range_start, r.name)
         for r in readouts
-    ])
+    ], to=to)
 
 
-def fit_particle_orbit(model, records, secmaps, range_start=None):
+def fit_particle_orbit(model, records, secmaps, range_start=None, to='#s'):
 
     (x, px, y, py), chi_squared, singular = fit_initial_orbit([
         (secmap[:, :6], secmap[:, 6], (record.posx, record.posy))
@@ -43,39 +42,26 @@ def fit_particle_orbit(model, records, secmaps, range_start=None):
     else:
         range_start = model.elements[range_start].name
 
-    backtw = model.backtrack(
-        range=range_start+'_reversed'+'/#e',
-        x=-x, y=y, px=px, py=-py)
+    elems = model.elements
+    if elems.index(to) < elems.index(range_start):
+        to_rev = '#e' if elems.index(to) == 0 else to + '_reversed'
+        backtw = model.backtrack(
+            range=range_start+'_reversed'+'/'+to_rev,
+            x=-x, y=y, px=px, py=-py)
+        data = {'s': backtw.s[-1] - backtw.s,
+                'x': -backtw.x,
+                'y': backtw.y}
+        tw0 = backtw[-1]
+        orbit = {'x': -tw0.x, 'px': tw0.px, 'y': tw0.y, 'py': -tw0.py}
 
-    data = {'s': backtw.s[-1] - backtw.s,
-            'x': -backtw.x,
-            'y': backtw.y}
-
-    tw0 = backtw[-1]
-    x, y, px, py = tw0.x, tw0.y, tw0.px, tw0.py
-
-    init_tw = {
-        'x': -x, 'px': px,
-        'y': y, 'py': -py,
-    }, chi_squared, singular
-
-    return init_tw, data
-
-
-def fit_forward_orbit(model, records, secmaps, range_start=None):
-    (x, px, y, py), chi_squared, singular = fit_initial_orbit([
-        (secmap[:, :6], secmap[:, 6], (record.posx, record.posy))
-        for record, secmap in zip(records, secmaps)
-    ])
-    if range_start is None:
-        range_start = records[0].name
     else:
-        range_start = model.elements[range_start].name
-    tw = model.twiss(
-        range=range_start+'/#e',
-        betx=1, bety=1,
-        x=x, y=y, px=px, py=py)
-    return (tw[-1], chi_squared, singular), tw
+        data = model.twiss(
+            range=range_start+'/'+to,
+            betx=1, bety=1,
+            x=x, y=y, px=px, py=py)
+        orbit = data[-1]
+
+    return (orbit, chi_squared, singular), data
 
 
 def fit_initial_orbit(records):
