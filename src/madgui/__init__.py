55fe8b70cc93cd0887bd576576a186dfa11f782f
Thomas Gläßle <t_glaessle@gmx.de>
2019-03-28 02:27:21 +0100

    Move most metadata to setup.cfg

--- src/madgui/__init__.py
+++ src/madgui/__init__.py
@@ -8,19 +8,11 @@ or simply:
     madgui
 """
 
+__version__ = '19.3.3'
+
 __title__ = 'madgui'
 __summary__ = 'GUI for accelerator simulations using MAD-X.'
 __uri__ = 'https://github.com/hibtc/madgui'
-
-__version__ = '19.3.3'
-
-__author__ = 'Thomas Gläßle'
-__email__ = 't_glaessle@gmx.de'
-
-__support__ = __email__
-
-__license__ = 'GPLv3+'
-__copyright__ = 'Copyright 2016-2019 HIT Betriebs GmbH'
 
 __credits__ = """
 madgui is developed for HIT Betriebs GmbH.
@@ -34,19 +26,6 @@ Special thanks to my supervisors for their help and support:
 - Rainer Cee
 - Andreas Peters
 """
-
-# Trove classifiers: https://pypi.python.org/pypi?:action=list_classifiers
-__classifiers__ = [
-    'Development Status :: 3 - Alpha',
-    'Environment :: X11 Applications :: Qt',
-    'Intended Audience :: Developers',
-    'Intended Audience :: Science/Research',
-    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-    'Operating System :: OS Independent',
-    'Programming Language :: Python :: 3',
-    'Topic :: Scientific/Engineering :: Medical Science Apps.',
-    'Topic :: Scientific/Engineering :: Physics',
-]
 
 
 def get_copyright_notice():
