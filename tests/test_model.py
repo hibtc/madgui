46938ca71a302f6b813f409022e5ada198cfa211
Thomas Gläßle <t_glaessle@gmx.de>
2019-05-13 17:09:55 +0200

    Fix broken test due to new hit_models version

--- tests/test_model.py
+++ tests/test_model.py
@@ -7,7 +7,7 @@ from madgui.model.madx import Model
 
 def test_load_model():
     model = Model.load_file(
-        'hit_models/hht3/hht3.cpymad.yml',
+        'hit_models/hht3.cpymad.yml',
         undo_stack=mock.Mock())
     assert model.seq_name == 'hht3'
 
