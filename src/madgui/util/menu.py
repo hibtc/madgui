e78237ad43206a7d032d9484758076e0871557d3
Thomas Gläßle <t_glaessle@gmx.de>
2019-05-22 15:33:27 +0200

    Add docstrings to madgui.util.menu

--- src/madgui/util/menu.py
+++ src/madgui/util/menu.py
@@ -1,5 +1,6 @@
 """
-Menu creation utilities.
+Utilities for defining a window menu in a more concice notation than manually
+creating the ``QMenuItem`` and assigning properties.
 """
 
 __all__ = [
@@ -16,6 +17,21 @@ from PyQt5.QtWidgets import QAction, QStyle
 
 class Item:
 
+    """Represents a menu item. Objects of this type will be realized as
+    ``QMenuItem``.
+
+    The following properties can be given:
+
+    :ivar str label: menu item label
+    :ivar str shortcut: shortcut, e.g. "Ctrl+S" (optional)
+    :ivar str description: tooltip and status bar message
+    :ivar callable callback: action to be taken when clicking the menu item
+    :ivar icon: icon, can be QStyle.StandardPixmap, an icon name defined in
+                the theme, or a QIcon
+    :ivar Bool enabled: whether the item is enabled (optional)
+    :ivar Bool checked: whether the item is checked (optional)
+    """
+
     def __init__(self, label, shortcut, description, callback, icon=None,
                  enabled=True, checked=None):
         self.label = label
@@ -29,6 +45,7 @@ class Item:
         self.checked = checked
 
     def action(self, parent):
+        """Create a ``QAction`` from this item."""
         checkable = self.checked is not None
         action = QAction(self.label, parent, checkable=checkable)
         if self.shortcut is not None:
@@ -51,12 +68,16 @@ class Item:
         return action
 
     def append_to(self, menu, parent=None):
+        """Append this item to the given menu."""
         action = self.action(parent)
         menu.addAction(action)
         return action
 
 
 def _set_from(slot, val):
+    """Connect a :class:`~madgui.util.collections.Bool` to the given slot.
+    This is used to keep the state of ``QAction.checked`` and ``.enabled``
+    properties synchronized."""
     if val is not None:
         try:
             cur = val()
@@ -67,6 +88,8 @@ def _set_from(slot, val):
 
 
 class Menu:
+
+    """(Sub-)menu to be inserted."""
 
     def __init__(self, label, items):
         self.label = label
@@ -79,6 +102,8 @@ class Menu:
 
 
 class Separator:
+
+    """Separator to be inserted in a menu."""
 
     @classmethod
     def append_to(cls, menu, parent):
